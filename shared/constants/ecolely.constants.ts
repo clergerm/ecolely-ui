1ba1edd4a48a670b0eb8b436d4c14845ddc4c0f4
Martin Clerger <clergerm@gmail.com>
2026-07-29 14:16:21 -0400

    Deploying new IP test

--- shared/constants/ecolely.constants.ts
+++ shared/constants/ecolely.constants.ts
@@ -3,6 +3,6 @@
  * Unauthorized copying, modification, distribution, or use of this software is strictly prohibited.
  */
 
-export const HOST_URL = 'http://localhost:8080';
-//export const HOST_URL = 'http://54.161.235.143:8080';
+//export const HOST_URL = 'http://localhost:8080';
+export const HOST_URL = 'http://50.16.28.130:8080';
 export const ROOT_API = '/api/ecolely-service';
